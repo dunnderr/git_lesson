b47f69a68269621f375e1d40aa930975861060fd
Derren Dunn <dunnderr@gmail.com>
2017-03-05 16:28:24 -0500

    changed the argument passed from x to y

--- src/my_square.py
+++ src/my_square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-def my_square(x):
+def my_square(y):
   """
-  my_square(x): calculates the square of arg x. Uses the python ** operator.
+  my_square(y): calculates the square of arg y. Uses the python ** operator.
   """
-  return(x**2)
+  return(y**2)
 
 if __name__ == "__main__":
   print("The square of 4 is {0:6.2f}".format(my_square(4)))
