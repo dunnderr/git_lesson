15e95e3f7897987be1e7d2593d805e48a9485e8a
dunnderr <dunnderr@gmail.com>
2017-03-05 16:21:02 -0500

    Update my_square.py
    Added comment and formatting to output.

--- src/my_square.py
+++ src/my_square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 def my_square(x):
+  """
+  my_square(x): calculates the square of arg x.
+  """
   return(x**2)
 
 if __name__ == "__main__":
-  print("The square of 3 is {0}".format(my_square(3)))
+  print("The square of 3 is {0:6.2f}".format(my_square(3)))
 
